415d912601b9c954ad02b55ad5fbfa8c39e0e75f
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-01-11 15:50:55 +0100

    checkpoint

--- index.js
+++ index.js
@@ -32,14 +32,14 @@ function mainView () {
 
 function Header () {
   return html`
-    <header class="flex justify-between pa4 pa5-l mb3 mb5-l">
-      <h1 class="f-1 black-70 ma0">
+    <header class="flex flex-column flex-row-ns justify-between pa4 pa5-l mb3 mb5-l">
+      <h1 class="f-1 black-70 ma0 lh-body">
         <a class="black-90 link" href="https://datproject.org">dat</a>
         <span class="">
           / desktop
         </span>
       </h1>
-      <nav class="flex mw5">
+      <nav class="flex-ns dn mw5 lh-body mt4 mt0-ns">
         ${Link('blog', 'https://blog.datarchive.com')}
         ${Link('registry', 'https://registry.datarchive.com')}
       </nav>
@@ -57,9 +57,9 @@ function Header () {
 
 function Headline () {
   return html`
-    <section class="flex justify-between pt0 ph4 ph5-l pb4-m pb5-l mb3 mb5-l">
-      <img class="mw6" src="data:image/png;base64,${fs.readFileSync(path.join(__dirname, 'assets/screenshot.png'), 'base64')}">
-      <div class="w-100 w-50-l">
+    <section class="flex flex-column flex-row-l justify-between pt0 pb4-m pb5-l mb3 mb5-l">
+      <img class="mw6 ph5-l" src="data:image/png;base64,${fs.readFileSync(path.join(__dirname, 'assets/screenshot.png'), 'base64')}">
+      <div class="w-100 w-50-l ph4 ph5-l">
         <h2 class="db lh-title f3 b mt0 dat-navy">
           Stream files from your computer to anywhere on the planet, securely and
           fast.
@@ -68,8 +68,7 @@ function Headline () {
           <a class="dn dib-ns ba no-underline br1 black-80 bg-gray-light b inline-flex items-center mr3 mv3 pv2 ph3">
             Windows (Soon)
           </a>
-          <a
-          href="/"
+          <a href="/"
             class="ba no-underline br1 black-80 bg-washed-blue grow b inline-flex items-center mr3 mv3 pv2 ph3">
             Download Now (MacOS)
           </a>
